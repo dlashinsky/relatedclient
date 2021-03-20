7b7e40b6e9cfba72b819e123f5f750aef5d02468
dlashinsky <da.lashinsky@gmail.com>
2021-03-20 09:30:04 -0700

    pushing boiler plate

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const rowdy = require('rowdy-logger')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+
+
+
+
+const app = express()
+const PORT = process.env.PORT || 3001
+const rowdyResults = rowdy.begin(app)
+
+app.use(morgan('tiny'))
+app.use(cors())
+
+
+app.use(express.json())
+
+
+
+app.listen(PORT, () =>{
+    rowdyResults.print()
+    console.log(`SHE up N' runnin' on dat 'dere port ${PORT}!  Get on wit it.  Life's a garden; DIG IT!`)
+})
