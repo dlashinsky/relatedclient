4f3f85676e29342732060e8953ccdd4939491f64
henry-mcdonald <a.henrymcdonald.a@gmail.com>
2021-03-21 16:07:38 -0500

    update seed data

--- server/seedDatabase.js
+++ server/seedDatabase.js
@@ -6,36 +6,37 @@ const Post = require('./models/Post')
 const Comment = require('./models/Comment')
 
 
-const topics = ["Wheelchair Accessability","Community Events",
-"Local Meetups", "Awareness"]  // list of sample topics
-const conditions = ["Cystic Fibrosis","Paralysis","Cerebral Palsy","Autism",
-"Chronic Bone Disease", "Cancer","Phenylketonuria","Other",
-"Surgical Complications"] // sample conditions
-const zips = [60657,60613,60614] // list of sample zips
-const relations = ["self","mother","father","caregiver","brother"]
+const topics = ["Wheelchair Accessability", "Community Events",
+    "Local Meetups", "Awareness"]  // list of sample topics
+const conditions = ["Cystic Fibrosis", "Paralysis", "Cerebral Palsy", "Autism",
+    "Chronic Bone Disease", "Cancer", "Phenylketonuria", "Other",
+    "Surgical Complications"] // sample conditions
+const zips = [60657, 60613, 60614] // list of sample zips
+const relations = ["self", "mother", "father", "caregiver", "brother"]
 
+faker.seed(123);
 
 
 //return single fake zip
-function createFakeZip(){
-    const randomZip = zips[Math.floor(Math.random()*zips.length)]
+function createFakeZip() {
+    const randomZip = zips[Math.floor(Math.random() * zips.length)]
     return randomZip
 }
 // createFakeZip();
 
 //return single fake relation
-function createFakeRelation(){
-    const randomRelation = relations[Math.floor(Math.random()*relations.length)]
+function createFakeRelation() {
+    const randomRelation = relations[Math.floor(Math.random() * relations.length)]
     return randomRelation
 }
 
 //return 1 up to n fake topics
-function createFakeTopics(){
+function createFakeTopics() {
     const userTopics = []
-    const numberOfSubscribedTopics = Math.ceil(Math.random()*topics.length)
-    for(let i = 0; i<numberOfSubscribedTopics; i++){
-        const randIndex = Math.floor(Math.random()*topics.length)
-        if(! userTopics.includes(topics[randIndex])){
+    const numberOfSubscribedTopics = Math.ceil(Math.random() * topics.length)
+    for (let i = 0; i < numberOfSubscribedTopics; i++) {
+        const randIndex = Math.floor(Math.random() * topics.length)
+        if (!userTopics.includes(topics[randIndex])) {
             userTopics.push(topics[randIndex])
         }
     }
@@ -44,22 +45,22 @@ function createFakeTopics(){
 // createFakeTopics();
 // returns generally 1 but sometimes 2 conditions
 
-function createFakeConditions(){
+function createFakeConditions() {
     const userConditions = []
-    let numberOfUserConditions = Math.ceil(Math.random()*conditions.length)
-    if(numberOfUserConditions>2) {
+    let numberOfUserConditions = Math.ceil(Math.random() * conditions.length)
+    if (numberOfUserConditions > 2) {
 
-       // make it somewhat unlikely to have 2 conditions
-        if(Math.random()>0.15){
+        // make it somewhat unlikely to have 2 conditions
+        if (Math.random() > 0.15) {
             numberOfUserConditions = 1
-        } else{
+        } else {
             numberOfUserConditions = 2
         }
 
     }
-    for(let i = 0; i<numberOfUserConditions; i++){
-        const randIndex = Math.floor(Math.random()*conditions.length)
-        if(! userConditions.includes(conditions[randIndex])){
+    for (let i = 0; i < numberOfUserConditions; i++) {
+        const randIndex = Math.floor(Math.random() * conditions.length)
+        if (!userConditions.includes(conditions[randIndex])) {
             userConditions.push(conditions[randIndex])
         }
     }
@@ -69,11 +70,11 @@ function createFakeConditions(){
 // createFakeConditions();
 
 
-async function createSomeUsers(n_records){
-    if(!n_records){
+async function createSomeUsers(n_records) {
+    if (!n_records) {
         n_records = 50 //default to create 50 dummy records
     }
-    for(let i=0;i<n_records;i++){
+    for (let i = 0; i < n_records; i++) {
         const record = await User.create({
             email: faker.internet.email(),
             username: faker.name.firstName(),
@@ -90,38 +91,82 @@ async function createSomeUsers(n_records){
 }
 // createSomeUsers();
 
-async function findARandomUser(){
+async function findARandomUserId() {
     // const count = await User.count()
     // console.log(count)
-    randomUser = await User.aggregate([{$match:{}},{$sample:{size:1}}])
-    console.log(randomUser)
+    randomUser = await User.aggregate([{ $match: {} }, { $sample: { size: 1 } }])
+    return randomUser[0]._id
 
 }
 
-async function createSomePosts(n_records){
-    if(!n_records){
+async function findSomeRandomUserIds() {
+    const maxNumberWhoLiked = 10
+    const numberWhoDidLike = Math.floor(Math.random()*maxNumberWhoLiked)
+    const idArray = []
+    for(i=0;i<numberWhoDidLike;i++){
+        newCandidate = await findARandomUserId()
+        if(!idArray.includes(newCandidate)){
+            idArray.push(newCandidate)
+        }
+    }
+    return idArray
+}
+
+async function findARandomPost(){
+    randomPost = await Post.aggregate([{ $match: {} }, { $sample: { size: 1 } }])
+    return randomPost[0]._id
+}
+
+
+
+async function createSomePosts(n_records) {
+    if (!n_records) {
         n_records = 20 //default to create 20 dummy posts
     }
 
-    let findUser = []
+    for (let i = 0; i < n_records; i++) {
+        let userId = await findARandomUserId()
+        let usersIdsWhoLiked = await findSomeRandomUserIds()
 
-    //find random user
-    for(let i = 0; i< n_records; i++){
-        findUser = await User.findOne({})
-        // console.log(findUser)
+
+        const makePost = await Post.create({
+            discussion_tags: createFakeTopics(),
+            content: faker.lorem.paragraph(Math.floor(Math.random()*5)),
+            user_id: userId,
+            users_who_liked: usersIdsWhoLiked
+
+        })
+        console.log(makePost)
     }
-
-    const makePost = await Post.create({
-        discussion_tags: createFakeTopics(),
-        content: faker.lorem.sentence(),
-        user_id: findUser._id
-    })
-    console.log(makePost)
 }
 
-//createSomePosts()
-findARandomUser()
+async function createSomeComments(n_records){
+    if(!n_records){
+        n_records = 30
+    }
+
+    for (let i = 0; i < n_records; i++) {
+        let userId = await findARandomUserId()
+        let usersIdsWhoLiked = await findSomeRandomUserIds()
+
+
+        const makePost = await Post.create({
+            discussion_tags: createFakeTopics(),
+            content: faker.lorem.paragraph(Math.floor(Math.random()*5)),
+            user_id: userId,
+            users_who_liked: usersIdsWhoLiked
+
+        })
+        console.log(makePost)
+    }
+}
+
+createSomeUsers()
 createSomePosts()
+
+
+
+//findARandomUserId()
 
 
 // for(i=0;i<50;i++){
